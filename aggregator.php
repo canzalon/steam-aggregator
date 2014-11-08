97490f916c14176f6d2d3d072f92422dbafeed50
Christopher Anzalone <canzalon@fau.edu>
2014-11-08 18:13:57 -0500

    Adding functionality to load name with games on aggregrator
    Adding functionality to load name with games on aggregrator

--- aggregator.php
+++ aggregator.php
@@ -5,6 +5,20 @@
 
 		<div class="container-fluid">
 
+				<!-- Users -->
+				<div class="row-fluid">
+					<div class="span12">
+						<?php
+
+							echo "<p>";
+							echo "<img src=" . $userArray[avatarfull] . " />";
+							echo "ID: " . $userArray[personaname] . "</p>";
+							echo "</p>";
+
+						?>
+					</div>
+				</div>
+
 				<!-- Filters -->
 				<div class="row-fluid">
 					<div class="span12">
@@ -15,6 +29,33 @@
 
 				<!-- Results -->
 				<div class="container">
+					<?php
+
+						$gameArrLength = count($gameArray);
+
+						// for ($x = 0; $x < $gameArrLength; $x++)
+						// {
+						// 	echo "<div class='row-fluid'>";
+						// 	echo "<div class='span2'>";
+						// 	echo gameArray[$x]['name'];
+						// 	echo "</div>";
+						// 	echo "</div>";
+						// }
+
+						foreach ($gameArray as $game)
+						{
+							echo "<div class='row-fluid'>";
+							echo "<div class='span2'>";
+							echo $game['name'];
+							echo "</div>";
+							echo "</div>";
+						}
+
+
+
+
+
+					?>
 
 					<!-- Header (let this be model for javascript produced html) -->
 					<div class="row-fluid">
@@ -27,8 +68,25 @@
 
 						</div>
 
+						<div class="span2">
+							
+						</div>
+
+						<div class="span2">
+							
+						</div>
+
+						<div class="span2">
+							
+						</div>
+
+						<div class="span2">
+							
+						</div>
 
 					</div>
+
+
 				</div>
 
 		</div>
