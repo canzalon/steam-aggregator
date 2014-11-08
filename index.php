c8b27207252b87882c3ce8f82e923021ad34b262
Christopher Anzalone <canzalon@fau.edu>
2014-11-08 14:42:02 -0500

    Including bootstrap css and js files

--- index.php
+++ index.php
@@ -6,7 +6,11 @@
 
         <title>Steam Aggregator</title>
 
-        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/bootstrap-responsive.css" />
+
+        <script language="JavaScript" src="js/bootstrap.js" type="text/javascript">
+        </script>
 
 	</head>
 
